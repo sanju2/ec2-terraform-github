1944fed58bd4ea8040e7b06c330b31dc92e4b5d4
lasantha96 <lsanjeewa947@gmail.com>
2022-10-15 23:47:08 +0400

    2022-10-15 commit test

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ terraform {
     }
   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
